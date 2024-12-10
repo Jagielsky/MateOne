1107d4e29f74994f5af8e501679521da9126a61b
Oskar Jagielski <94719723+Jagielsky@users.noreply.github.com>
2024-12-10 02:06:07 +0100

    basic chess board implementation

---
+++ Interface/script.js
@@ -0,0 +1,7 @@
+var config = {
+    pieceTheme: '../Data/chessboardjs-1.0.0/img/chesspieces/wikipedia/{piece}.svg',
+    position: 'start',
+    draggable: true
+}
+
+var board = Chessboard('board', config)
