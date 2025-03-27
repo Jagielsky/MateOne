eaf6e69710abd44e57ba424dc661f5b8edea0cc3
Oskar Jagielski <94719723+Jagielsky@users.noreply.github.com>
2025-03-27 15:46:29 +0100

    some fixes

--- engine.py
+++ engine.py
@@ -8,15 +8,3 @@ def engine_response(fen):
     random_move = random.choice(legal_moves)
     board.push(random_move)
     return {'fen': board.fen()}
-
-#dodaj ocene pozycji
-#każda figura powinna mieć swoją wartość, król najważniejszy
-#sumuj je i odemij wartosci biale od czarnych wtedy wiadomo kto ma przewage
-#kazda figura ma lepsze i gorsze pola, je też wliczaj do oceny
-
-#poczytaj o bitboard, musisz jakoś przedstawiac szachownice dla silnika zeby bylo wydajnie, na koncu i tak zwróci fen
-
-#... tutaj zaczynają się schody
-
-#algorytm minmax i przycinanie alfa beta czyli przeszukiwanie każdego możliwego ruchu (głebia określa ile do przodu)
-#wybranie najlepszego ruchu po ocenie
--- static/js/script.js
+++ static/js/script.js
@@ -1,50 +1,52 @@
-var board = null
-var game = new Chess()
+var board = null;
+var game = new Chess();
 
-function onDragStart (source, piece, position, orientation) {
-  if (game.game_over()) return false
+function onDragStart(source, piece, position, orientation) {
+  if (game.game_over()) return false;
 
-  if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false
+  if (
+    (game.turn() === "w" && piece.search(/^b/) !== -1) ||
+    (game.turn() === "b" && piece.search(/^w/) !== -1)
+  ) {
+    return false;
   }
 }
 
-function onDrop (source, target) {
+function onDrop(source, target) {
   var move = game.move({
     from: source,
     to: target,
-    promotion: 'q'
-  })
+    promotion: "q",
+  });
 
-  if (move === null) return 'snapback'
+  if (move === null) return "snapback";
 }
 
 function onSnapEnd() {
   board.position(game.fen());
 
   // Wysyłanie stanu planszy do backendu
-  fetch('/fen', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+  fetch("/fen", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ fen: game.fen() }),
   })
-  .then(response => response.json())
-  .then(data => {
-    game.load(data.fen);
-    board.position(data.fen);
-  });
+    .then((response) => response.json())
+    .then((data) => {
+      game.load(data.fen);
+      board.position(data.fen);
+    });
 }
 
 //Konfiguracja szachownicy chessboardjs
 var config = {
-  pieceTheme: pieceUrl + '{piece}.svg',
+  pieceTheme: pieceUrl + "{piece}.svg",
   draggable: true,
-  position: 'start',
+  position: "start",
   onDragStart: onDragStart,
   onDrop: onDrop,
-  onSnapEnd: onSnapEnd
-}
+  onSnapEnd: onSnapEnd,
+};
 
 //Wygenerowanie szachownicy z konfiguracją
-board = Chessboard('board', config)
+board = Chessboard("board", config);
